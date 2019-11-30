8b69f37da42afa5014330b7e4de4a50d698f1675
Fedya <fedor.chuprakov@mail.ru>
2019-12-01 00:10:00 +0300

    first commit

---
+++ cmake-build-debug/CMakeFiles/lsof.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lsof.dir/main.c.o"
+  "lsof.pdb"
+  "lsof"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/lsof.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
